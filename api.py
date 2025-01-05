75efcb4d21344ab146f883a41cbe3e779709db89
Hoang Hong Quan <lzhoang2302@gmail.com>
2025-01-05 11:17:28 +0700

    Reduce unnecessary requests

--- api.py
+++ api.py
@@ -3,6 +3,8 @@ import urllib.request
 import json
 import wca_vote_crawler
 from flask_cors import CORS
+import threading
+import time
 
 class VoteAPI:
 
@@ -47,19 +49,29 @@ class FlaskApp:
     def __init__(self):
         self.app = Flask(__name__)
         self.vote_api = VoteAPI()
+        self.cached_vote_data = {}
         self._register_routes()
-        CORS(self.app, resources={r"/*": {"origins": ["https://lzhoang2801.github.io/", "http://127.0.0.1:5500"]}})
+        CORS(self.app, resources={r"/*": {"origins": ["https://lzhoang2801.github.io", "http://127.0.0.1:5500"]}})
+        
+        self.update_thread = threading.Thread(target=self._update_votes_periodically, daemon=True)
+        self.update_thread.start()
+
+    def _update_votes_periodically(self):
+        while True:
+            try:
+                self.cached_vote_data = self.vote_api.get_votes()
+                time.sleep(1)
+            except Exception as e:
+                print(f"Update Error: {e}")
 
     def _register_routes(self):
         self.app.route('/get_votes')(self.api_get_votes)
 
     def api_get_votes(self):
         try:
-            vote_data = self.vote_api.get_votes()
-
-            if vote_data:
-                return jsonify(vote_data), 200
-            return jsonify({"error": "Could not retrieve vote data"}), 500
+            if self.cached_vote_data:
+                return jsonify(self.cached_vote_data), 200
+            return jsonify({"error": "No vote data available"}), 500
         except Exception as e:
             print(f"API Error: {e}")
             return jsonify({"error": "An unexpected error occurred"}), 500
